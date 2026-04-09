2ea04959e7aad91ff478998989ca2885f2ae51f1
Jefferson Osberto Salazar Escobar <jsalazare4@miumg.edu.gt>
2026-04-08 21:32:27 -0600

    Agrego validaciones y calculo total en Factura

--- src/encapsulamiento/Factura.java
+++ src/encapsulamiento/Factura.java
@@ -5,4 +5,40 @@ public class Factura {
     private String descripcion;
     private int cantidad;
     private double precioUnitario;
+
+    public void setCodigoFactura(String codigoFactura) {
+        this.codigoFactura = codigoFactura;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setCantidad(int cantidad) {
+        if (cantidad > 0) {
+            this.cantidad = cantidad;
+        } else {
+            System.out.println("Cantidad invalida");
+        }
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        if (precioUnitario > 0) {
+            this.precioUnitario = precioUnitario;
+        } else {
+            System.out.println("Precio invalido");
+        }
+    }
+
+    public double calcularTotal() {
+        return cantidad * precioUnitario;
+    }
+
+    public void mostrarFactura() {
+        System.out.println("Codigo: " + codigoFactura);
+        System.out.println("Descripcion: " + descripcion);
+        System.out.println("Cantidad: " + cantidad);
+        System.out.println("Precio Unitario: " + precioUnitario);
+        System.out.println("Total: " + calcularTotal());
+    }
 }
