3a37eeffeed3b549cd702aede0088b446afabf87
Jefferson Osberto Salazar Escobar <jsalazare4@miumg.edu.gt>
2026-04-08 21:18:19 -0600

    Agrego validaciones de telefono y email en Contacto

--- src/encapsulamiento/Contacto.java
+++ src/encapsulamiento/Contacto.java
@@ -4,4 +4,28 @@ public class Contacto {
     private String nombre;
     private String telefono;
     private String email;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        if (telefono.length() == 8) {
+            this.telefono = telefono;
+        } else {
+            System.out.println("Telefono invalido");
+        }
+    }
+
+    public void setEmail(String email) {
+        if (email.contains("@")) {
+            this.email = email;
+        } else {
+            System.out.println("Email invalido");
+        }
+    }
+
+    public void mostrarContacto() {
+        System.out.println(nombre + " - " + telefono + " - " + email);
+    }
 }
