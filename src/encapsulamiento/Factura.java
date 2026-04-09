c5743622e2b1238327d0ee9defeefeee86c476bf
Jefferson Osberto Salazar Escobar <jsalazare4@miumg.edu.gt>
2026-04-08 21:31:56 -0600

    Creo clase Factura con atributos

---
+++ src/encapsulamiento/Factura.java
@@ -0,0 +1,8 @@
+package encapsulamiento;
+
+public class Factura {
+    private String codigoFactura;
+    private String descripcion;
+    private int cantidad;
+    private double precioUnitario;
+}
