4cfe91a6d26a4819972d30f986d49fc746608d42
Jefferson Osberto Salazar Escobar <jsalazare4@miumg.edu.gt>
2026-04-08 21:26:16 -0600

    Agrego logica de actualizacion de puntaje en Jugador

--- src/encapsulamiento/Jugador.java
+++ src/encapsulamiento/Jugador.java
@@ -4,4 +4,20 @@ public class Jugador {
     private String nombre;
     private int puntajeActual;
     private int puntajeMaximo;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void actualizarPuntaje(int nuevoPuntaje) {
+        if (nuevoPuntaje >= 0) {
+            puntajeActual = nuevoPuntaje;
+
+            if (nuevoPuntaje > puntajeMaximo) {
+                puntajeMaximo = nuevoPuntaje;
+            }
+        } else {
+            System.out.println("Puntaje invalido");
+        }
+    }
 }
