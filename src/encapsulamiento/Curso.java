371806fd629b955e8e5e060318137f98fcbbc0ad
Jefferson Osberto Salazar Escobar <jsalazare4@miumg.edu.gt>
2026-04-08 20:51:40 -0600

    Agrego validación de notas y métodos promedio y estado en Curso

--- src/encapsulamiento/Curso.java
+++ src/encapsulamiento/Curso.java
@@ -5,4 +5,44 @@ public class Curso {
     private double nota1;
     private double nota2;
     private double nota3;
+
+    public void setNombreCurso(String nombreCurso) {
+        this.nombreCurso = nombreCurso;
+    }
+
+    public void setNota1(double nota1) {
+        if (nota1 >= 0 && nota1 <= 100) {
+            this.nota1 = nota1;
+        } else {
+            System.out.println("Nota inválida");
+        }
+    }
+
+    public void setNota2(double nota2) {
+        if (nota2 >= 0 && nota2 <= 100) {
+            this.nota2 = nota2;
+        } else {
+            System.out.println("Nota inválida");
+        }
+    }
+
+    public void setNota3(double nota3) {
+        if (nota3 >= 0 && nota3 <= 100) {
+            this.nota3 = nota3;
+        } else {
+            System.out.println("Nota inválida");
+        }
+    }
+
+    public double promedio() {
+        return (nota1 + nota2 + nota3) / 3;
+    }
+
+    public String estado() {
+        if (promedio() >= 61) {
+            return "Aprobado";
+        } else {
+            return "Reprobado";
+        }
+    }
 }
