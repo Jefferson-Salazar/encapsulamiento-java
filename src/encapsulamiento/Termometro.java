c0b2e077404b2ad28437c10c0f68f1c294df4ba0
Jefferson Osberto Salazar Escobar <jsalazare4@miumg.edu.gt>
2026-04-08 21:04:31 -0600

    Agrego validacion de temperatura y conversion a Fahrenheit en Termometro

--- src/encapsulamiento/Termometro.java
+++ src/encapsulamiento/Termometro.java
@@ -2,4 +2,16 @@ package encapsulamiento;
 
 public class Termometro {
     private double celsius;
+
+    public void setCelsius(double temperatura) {
+        if (temperatura >= -273.15) {
+            this.celsius = temperatura;
+        } else {
+            System.out.println("Temperatura invalida");
+        }
+    }
+
+    public double getFahrenheit() {
+        return (celsius * 9/5) + 32;
+    }
 }
